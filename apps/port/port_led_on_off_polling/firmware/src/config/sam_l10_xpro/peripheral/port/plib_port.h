2fe44280c2e285bc56596647f14cb105cfb76f7d
Vishal Gandhi <vishal.gandhi@microchip.com>
2020-08-24 10:52:12 +0530

    [PORT] Regenerated demo for port_port_led_on_off_polling_sam_l10_xpro
    MH3-41214
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/port/port_led_on_off_polling/firmware/src/config/sam_l10_xpro/peripheral/port/plib_port.h
+++ apps/port/port_led_on_off_polling/firmware/src/config/sam_l10_xpro/peripheral/port/plib_port.h
@@ -65,27 +65,23 @@
 // *****************************************************************************
 // *****************************************************************************
 
-  
 /*** Macros for LED pin ***/
 #define LED_Set()               (PORT_REGS->GROUP[0].PORT_OUTSET = 1 << 7)
 #define LED_Clear()             (PORT_REGS->GROUP[0].PORT_OUTCLR = 1 << 7)
 #define LED_Toggle()            (PORT_REGS->GROUP[0].PORT_OUTTGL = 1 << 7)
-#define LED_Get()               (((PORT_REGS->GROUP[0].PORT_IN >> 7)) & 0x01)
 #define LED_OutputEnable()      (PORT_REGS->GROUP[0].PORT_DIRSET = 1 << 7)
 #define LED_InputEnable()       (PORT_REGS->GROUP[0].PORT_DIRCLR = 1 << 7)
+#define LED_Get()               (((PORT_REGS->GROUP[0].PORT_IN >> 7)) & 0x01)
 #define LED_PIN                  PORT_PIN_PA07
 
 /*** Macros for SWITCH pin ***/
 #define SWITCH_Set()               (PORT_REGS->GROUP[0].PORT_OUTSET = 1 << 27)
 #define SWITCH_Clear()             (PORT_REGS->GROUP[0].PORT_OUTCLR = 1 << 27)
 #define SWITCH_Toggle()            (PORT_REGS->GROUP[0].PORT_OUTTGL = 1 << 27)
-#define SWITCH_Get()               (((PORT_REGS->GROUP[0].PORT_IN >> 27)) & 0x01)
 #define SWITCH_OutputEnable()      (PORT_REGS->GROUP[0].PORT_DIRSET = 1 << 27)
 #define SWITCH_InputEnable()       (PORT_REGS->GROUP[0].PORT_DIRCLR = 1 << 27)
+#define SWITCH_Get()               (((PORT_REGS->GROUP[0].PORT_IN >> 27)) & 0x01)
 #define SWITCH_PIN                  PORT_PIN_PA27
-
-
-
 // *****************************************************************************
 /* PORT Group
 
@@ -811,7 +807,7 @@ void PORT_GroupToggle(PORT_GROUP group, uint32_t mask);
     void PORT_GroupInputEnable(PORT_GROUP group, uint32_t mask)
 
   Summary:
-    Confgiures the selected IO pins of a group as input.
+    Configures the selected IO pins of a group as input.
 
   Description:
     This function configures the selected IO pins of a group as input. The pins
@@ -850,7 +846,7 @@ void PORT_GroupInputEnable(PORT_GROUP group, uint32_t mask);
     void PORT_GroupOutputEnable(PORT_GROUP group, uint32_t mask)
 
   Summary:
-    Confgiures the selected IO pins of a group as output.
+    Configures the selected IO pins of a group as output.
 
   Description:
     This function configures the selected IO pins of a group as output. The pins
