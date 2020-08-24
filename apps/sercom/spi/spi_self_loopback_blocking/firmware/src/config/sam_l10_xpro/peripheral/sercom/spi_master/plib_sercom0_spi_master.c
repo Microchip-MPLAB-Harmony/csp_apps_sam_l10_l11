ef6da5de760ac794544731ba1f0ac141a50e5ac3
Vishal Gandhi <vishal.gandhi@microchip.com>
2020-08-24 11:02:25 +0530

    [SERCOM] Regenerated demo for sercom_spi_spi_self_loopback_blocking_sam_l10_xpro
    MH3-41232
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/sercom/spi/spi_self_loopback_blocking/firmware/src/config/sam_l10_xpro/peripheral/sercom/spi_master/plib_sercom0_spi_master.c
+++ apps/sercom/spi/spi_self_loopback_blocking/firmware/src/config/sam_l10_xpro/peripheral/sercom/spi_master/plib_sercom0_spi_master.c
@@ -5,10 +5,10 @@
     Microchip Technology Inc.
 
   File Name
-    plib_sercom0_spi.c
+    plib_sercom0_spi_master.c
 
   Summary
-    SERCOM0_SPI PLIB Implementation File.
+    SERCOM0_SPI Master PLIB Implementation File.
 
   Description
     This file defines the interface to the SERCOM SPI peripheral library.
@@ -45,7 +45,7 @@
 *******************************************************************************/
 // DOM-IGNORE-END
 
-#include "plib_sercom0_spi.h"
+#include "plib_sercom0_spi_master.h"
 
 // *****************************************************************************
 // *****************************************************************************
@@ -159,12 +159,14 @@ bool SERCOM0_SPI_TransferSetup(SPI_TRANSFER_SETUP *setup, uint32_t spiSourceCloc
         if((baudValue > 0) & (baudValue <= 255))
         {
             /* Selection of the Clock Polarity and Clock Phase */
+			SERCOM0_REGS->SPIM.SERCOM_CTRLA &= ~(SERCOM_SPIM_CTRLA_CPOL_Msk | SERCOM_SPIM_CTRLA_CPHA_Msk);
             SERCOM0_REGS->SPIM.SERCOM_CTRLA |= (uint32_t)setup->clockPolarity | (uint32_t)setup->clockPhase;
 
             /* Selection of the Baud Value */
             SERCOM0_REGS->SPIM.SERCOM_BAUD = baudValue;
 
             /* Selection of the Character Size */
+			SERCOM0_REGS->SPIM.SERCOM_CTRLB &= ~SERCOM_SPIM_CTRLB_CHSIZE_Msk;
             SERCOM0_REGS->SPIM.SERCOM_CTRLB |= setup->dataBits;
 
             /* Wait for synchronization */
