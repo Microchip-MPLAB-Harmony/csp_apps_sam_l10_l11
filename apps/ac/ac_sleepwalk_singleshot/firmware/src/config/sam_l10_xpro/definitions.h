45f01712b9f207ef8e28191a4a9f8f534f700c2a
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2021-01-28 22:18:06 +0530

    [AC] Regenerated demo for ac_ac_sleepwalk_singleshot_sam_l10_xpro
    MH3-49347
    
    Signed-off-by: Jignesh Moradiya <jignesh.moradiya@microchip.com>

--- apps/ac/ac_sleepwalk_singleshot/firmware/src/config/sam_l10_xpro/definitions.h
+++ apps/ac/ac_sleepwalk_singleshot/firmware/src/config/sam_l10_xpro/definitions.h
@@ -52,10 +52,10 @@
 #include "peripheral/nvmctrl/plib_nvmctrl.h"
 #include "peripheral/evsys/plib_evsys.h"
 #include "peripheral/sercom/usart/plib_sercom0_usart.h"
+#include "peripheral/pm/plib_pm.h"
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
-#include "peripheral/pm/plib_pm.h"
 #include "peripheral/ac/plib_ac.h"
 #include "peripheral/rtc/plib_rtc.h"
 
@@ -66,6 +66,9 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* CPU clock frequency */
+#define CPU_CLOCK_FREQUENCY 32000000
 
 // *****************************************************************************
 // *****************************************************************************
