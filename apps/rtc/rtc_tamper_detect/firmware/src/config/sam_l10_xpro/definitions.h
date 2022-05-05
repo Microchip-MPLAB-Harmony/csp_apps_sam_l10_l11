e53fae4641274045d339618c6f2bd02196185cea
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2022-05-05 14:05:15 +0530

    [RTC] Regenerated demo for rtc_rtc_tamper_detect_sam_l10_xpro
    MH3-68056
    
    Signed-off-by: Jignesh Moradiya <jignesh.moradiya@microchip.com>

--- apps/rtc/rtc_tamper_detect/firmware/src/config/sam_l10_xpro/definitions.h
+++ apps/rtc/rtc_tamper_detect/firmware/src/config/sam_l10_xpro/definitions.h
@@ -50,12 +50,12 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include "peripheral/nvmctrl/plib_nvmctrl.h"
-#include "peripheral/evsys/plib_evsys.h"
 #include "peripheral/sercom/usart/plib_sercom0_usart.h"
-#include "peripheral/pm/plib_pm.h"
+#include "peripheral/evsys/plib_evsys.h"
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
+#include "peripheral/pm/plib_pm.h"
 #include "peripheral/rtc/plib_rtc.h"
 
 // DOM-IGNORE-BEGIN
--- apps/rtc/rtc_tamper_detect/firmware/src/config/sam_l10_xpro/peripheral/port/plib_port.c
+++ apps/rtc/rtc_tamper_detect/firmware/src/config/sam_l10_xpro/peripheral/port/plib_port.c
@@ -52,7 +52,6 @@
 // Section: PORT Implementation
 // *****************************************************************************
 // *****************************************************************************
-
 // *****************************************************************************
 /* Function:
 
@@ -78,11 +77,12 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[0].PORT_PINCFG[24] = 0x1;
    PORT_REGS->GROUP[0].PORT_PINCFG[25] = 0x1;
 
+   PORT_REGS->GROUP[0].PORT_PMUX[3] = 0x0;
    PORT_REGS->GROUP[0].PORT_PMUX[4] = 0x6;
    PORT_REGS->GROUP[0].PORT_PMUX[12] = 0x22;
 
-}
 
+}
 // *****************************************************************************
 /* Function:
     uint32_t PORT_GroupRead(PORT_GROUP group)
