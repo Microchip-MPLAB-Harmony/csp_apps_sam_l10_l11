bf54a3671a1015933803201354a385aa988be486
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2022-05-05 14:17:13 +0530

    [SERCOM] Regenerated demo for sercom_spi_spi_ping_pong_with_dma_sam_l10_xpro
    MH3-68060
    
    Signed-off-by: Jignesh Moradiya <jignesh.moradiya@microchip.com>

--- apps/sercom/spi/spi_ping_pong_with_dma/firmware/src/config/sam_l10_xpro/definitions.h
+++ apps/sercom/spi/spi_ping_pong_with_dma/firmware/src/config/sam_l10_xpro/definitions.h
@@ -49,13 +49,13 @@
 #include <stddef.h>
 #include <stdbool.h>
 #include "peripheral/nvmctrl/plib_nvmctrl.h"
-#include "peripheral/evsys/plib_evsys.h"
 #include "peripheral/sercom/spi_master/plib_sercom0_spi_master.h"
-#include "peripheral/pm/plib_pm.h"
+#include "peripheral/evsys/plib_evsys.h"
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/dmac/plib_dmac.h"
+#include "peripheral/pm/plib_pm.h"
 
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
