d3ab44f3f7fce442d3e07686ff49cf6ac369070b
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2022-05-05 14:00:20 +0530

    [RTC] Regenerated demo for rtc_rtc_alarm_sam_l10_xpro
    MH3-68054
    
    Signed-off-by: Jignesh Moradiya <jignesh.moradiya@microchip.com>

--- apps/rtc/rtc_alarm/firmware/src/config/sam_l10_xpro/peripheral/rtc/plib_rtc.h
+++ apps/rtc/rtc_alarm/firmware/src/config/sam_l10_xpro/peripheral/rtc/plib_rtc.h
@@ -94,13 +94,11 @@ typedef enum
 
 typedef uint32_t RTC_CLOCK_INT_MASK;
 
-typedef enum
-{
-    TAMPER_CHANNEL_0 = 0U,
-    TAMPER_CHANNEL_1 = 1U,
-    TAMPER_CHANNEL_2 = 2U,
-    TAMPER_CHANNEL_3 = 3U,
-} TAMPER_CHANNEL;
+ #define   TAMPER_CHANNEL_0  (0U)
+ #define   TAMPER_CHANNEL_1  (1U)
+ #define   TAMPER_CHANNEL_2  (2U)
+ #define   TAMPER_CHANNEL_3  (3U)
+typedef uint32_t TAMPER_CHANNEL;
 typedef void (*RTC_CALLBACK)( RTC_CLOCK_INT_MASK intCause, uintptr_t context );
 
 typedef struct
@@ -112,6 +110,8 @@ typedef struct
 } RTC_OBJECT;
 
 void RTC_Initialize(void);
+void RTC_RTCCClockSyncEnable ( void );
+void RTC_RTCCClockSyncDisable ( void );
 bool RTC_RTCCTimeSet (struct tm * initialTime );
 void RTC_RTCCTimeGet ( struct tm * currentTime );
 bool RTC_RTCCAlarmSet ( struct tm * alarmTime, RTC_ALARM_MASK mask );
