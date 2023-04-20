ba7d579b83a3ce462e08cfe225a73a63a465f040
Prachi Phadke <prachi.phadke@microchip.com>
2023-04-20 12:02:01 +0530

    [TC] Regenerated demo for tc_tc_compare_mode_sam_l10_xpro
    MH3-79657

--- apps/tc/tc_compare_mode/firmware/src/config/sam_l10_xpro/peripheral/port/plib_port.c
+++ apps/tc/tc_compare_mode/firmware/src/config/sam_l10_xpro/peripheral/port/plib_port.c
@@ -71,13 +71,13 @@
 void PORT_Initialize(void)
 {
    /************************** GROUP 0 Initialization *************************/
-   PORT_REGS->GROUP[0].PORT_PINCFG[5] = 0x1;
-   PORT_REGS->GROUP[0].PORT_PINCFG[18] = 0x1;
-   PORT_REGS->GROUP[0].PORT_PINCFG[25] = 0x1;
+   PORT_REGS->GROUP[0].PORT_PINCFG[5] = 0x1U;
+   PORT_REGS->GROUP[0].PORT_PINCFG[18] = 0x1U;
+   PORT_REGS->GROUP[0].PORT_PINCFG[25] = 0x1U;
 
-   PORT_REGS->GROUP[0].PORT_PMUX[2] = 0x40;
-   PORT_REGS->GROUP[0].PORT_PMUX[9] = 0x4;
-   PORT_REGS->GROUP[0].PORT_PMUX[12] = 0x40;
+   PORT_REGS->GROUP[0].PORT_PMUX[2] = 0x40U;
+   PORT_REGS->GROUP[0].PORT_PMUX[9] = 0x4U;
+   PORT_REGS->GROUP[0].PORT_PMUX[12] = 0x40U;
 
 
 }
