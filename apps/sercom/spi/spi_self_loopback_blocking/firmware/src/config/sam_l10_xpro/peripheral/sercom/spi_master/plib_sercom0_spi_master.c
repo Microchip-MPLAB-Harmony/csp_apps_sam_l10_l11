147551608d44a2086284dc008b38be80bae4e652
Pranjal Das <pranjal.das@microchip.com>
2024-10-17 10:52:10 +0530

    [SERCOM] Regenerated demo for sercom__spi__spi_self_loopback_blocking__firmware__sam_l10_xpro.X
    MH3-102198

--- apps/sercom/spi/spi_self_loopback_blocking/firmware/src/config/sam_l10_xpro/peripheral/sercom/spi_master/plib_sercom0_spi_master.c
+++ apps/sercom/spi/spi_self_loopback_blocking/firmware/src/config/sam_l10_xpro/peripheral/sercom/spi_master/plib_sercom0_spi_master.c
@@ -157,6 +157,7 @@ bool SERCOM0_SPI_TransferSetup(SPI_TRANSFER_SETUP *setup, uint32_t spiSourceCloc
     /* Disable the SPI Module */
     SERCOM0_REGS->SPIM.SERCOM_CTRLA &= ~(SERCOM_SPIM_CTRLA_ENABLE_Msk);
 
+
     /* Wait for synchronization */
     while((SERCOM0_REGS->SPIM.SERCOM_SYNCBUSY) != 0U)
     {
