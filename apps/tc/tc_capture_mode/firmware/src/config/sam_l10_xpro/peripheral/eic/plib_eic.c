4bc4c5716ba86c729fddef4497fcd3e27acd265c
Pranjal Das <pranjal.das@microchip.com>
2024-10-17 10:46:32 +0530

    [TC] Regenerated demo for tc__tc_capture_mode__firmware__sam_l10_xpro.X
    MH3-102212

--- apps/tc/tc_capture_mode/firmware/src/config/sam_l10_xpro/peripheral/eic/plib_eic.c
+++ apps/tc/tc_capture_mode/firmware/src/config/sam_l10_xpro/peripheral/eic/plib_eic.c
@@ -79,6 +79,7 @@ void EIC_Initialize (void)
 
     /* NMI Control register */
 
+
     /* Interrupt sense type and filter control for EXTINT channels 0 to (8-1) */
     EIC_REGS->EIC_CONFIG =  EIC_CONFIG_SENSE0_NONE 
         | EIC_CONFIG_SENSE1_NONE 
@@ -88,6 +89,7 @@ void EIC_Initialize (void)
         | EIC_CONFIG_SENSE5_NONE 
         | EIC_CONFIG_SENSE6_NONE 
         | EIC_CONFIG_SENSE7_NONE ;
+
 
 
 
